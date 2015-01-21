d51ff13190a249f3127c00ab41068a8786b4804d
Yaman Umuroglu <maltanar@gmail.com>
2015-01-21 13:27:47 +0100

    add more config to the RequestGenerator

--- backend/RequestGenerator.scala
+++ backend/RequestGenerator.scala
@@ -7,15 +7,30 @@ import AXIStreamDefs._
 import AXIDefs._
 
 class RequestGenerator() extends Module {
-
-  val io = new Bundle {
-    // TODO make these configurable from some global cfg
-    val addrBits = 32
-    val idBits = 3
-    val colLenBurstSize = 8
-    val riBurstSize = 8
-    val dvBurstSize = 4
+  // TODO make these configurable from some global cfg
+  val addrBits = 32
+  val idBits = 2
+  val mmapDataBits = 64       // width of AXI data bus
+  val colLenDataBits = 32     // width of each column length
+  val rowIndDataBits = 32     // width of each row index
+  val dvDataBits = 1          // width of each dense vector element
   
+  // calculate elements per beat for the various components
+  val colLenPerBeat = mmapDataBits / colLenDataBits
+  val rowIndPerBeat = mmapDataBits / rowIndDataBits
+  val dvPerBeat = mmapDataBits / dvDataBits
+  
+  // define burst sizes for the various components
+  val colLenBurstSize = 8
+  val riBurstSize = 8
+  val dvBurstSize = 1
+  
+  // calculate elements per burst for the various components
+  val colLenPerBurst = colLenBurstSize * colLenPerBeat
+  val rowIndPerBurst = rowIndBurstSize * rowIndPerBeat
+  val dvPerBurst = dvBurstSize * dvPerBeat
+    
+  val io = new Bundle {
     // control interface
     val start = Bool(INPUT)
     val colStart = UInt(INPUT, 32)
@@ -39,8 +54,6 @@ class RequestGenerator() extends Module {
   val regColPtr = Reg(init = UInt(0, 32))
   val regRowIndPtr = Reg(init = UInt(0, 32))
   val regDVPtr = Reg(init = UInt(0, 32))
-  
-
   
   // state machine definitions
   val sIdle :: sReqColLen :: sReqRowInd :: sReqDenVec :: Nil = Enum(UInt(), 4)
