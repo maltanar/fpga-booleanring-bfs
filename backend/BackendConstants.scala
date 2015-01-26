efed4d8f47b103c2d84847b6a2a30b0f54952612
Yaman Umuroglu <maltanar@gmail.com>
2015-01-26 11:45:01 +0100

    move backend constants to singleton+trait

---
+++ backend/BackendConstants.scala
@@ -0,0 +1,35 @@
+package BFSBackend
+
+trait BackendConstants {   
+  val addrBits = 32           // width of address for AXI master
+  val idBits = 2              // number of ID bits for AXI data bus
+  val mmapDataBits = 64       // width of AXI data bus
+  val colLenDataBits = 32     // width of each column length
+  val rowIndDataBits = 32     // width of each row index
+  val dvDataBits = 1          // width of each dense vector element
+
+  // define IDs for memory requests
+  val colLenReqID = 0
+  val rowIndReqID = 1
+  val dvReqID = 2
+
+  // calculate elements per beat for the various components
+  val colLenPerBeat = mmapDataBits / colLenDataBits
+  val rowIndPerBeat = mmapDataBits / rowIndDataBits
+  val dvPerBeat = mmapDataBits / dvDataBits
+
+  // define burst sizes for the various components
+  val colLenBurstSize = 8
+  val rowIndBurstSize = 8
+  val dvBurstSize = 1
+
+  // calculate elements per burst for the various components
+  val colLenPerBurst = colLenBurstSize * colLenPerBeat
+  val rowIndPerBurst = rowIndBurstSize * rowIndPerBeat
+  val dvPerBurst = dvBurstSize * dvPerBeat
+}
+
+object Constants extends
+   BackendConstants
+{
+}
--- backend/RequestGenerator.scala
+++ backend/RequestGenerator.scala
@@ -5,36 +5,9 @@ import Literal._
 import Node._
 import AXIStreamDefs._
 import AXIDefs._
+import Constants._
 
 class RequestGenerator() extends Module {
-  // TODO make these configurable from some global cfg
-  val addrBits = 32
-  val idBits = 2
-  val mmapDataBits = 64       // width of AXI data bus
-  val colLenDataBits = 32     // width of each column length
-  val rowIndDataBits = 32     // width of each row index
-  val dvDataBits = 1          // width of each dense vector element
-  
-  // define IDs for memory requests
-  val colLenReqID = 0
-  val rowIndReqID = 1
-  val dvReqID = 2
-  
-  // calculate elements per beat for the various components
-  val colLenPerBeat = mmapDataBits / colLenDataBits
-  val rowIndPerBeat = mmapDataBits / rowIndDataBits
-  val dvPerBeat = mmapDataBits / dvDataBits
-  
-  // define burst sizes for the various components
-  val colLenBurstSize = 8
-  val rowIndBurstSize = 8
-  val dvBurstSize = 1
-  
-  // calculate elements per burst for the various components
-  val colLenPerBurst = colLenBurstSize * colLenPerBeat
-  val rowIndPerBurst = rowIndBurstSize * rowIndPerBeat
-  val dvPerBurst = dvBurstSize * dvPerBeat
-    
   val io = new Bundle {
     // control interface
     val start = Bool(INPUT)
