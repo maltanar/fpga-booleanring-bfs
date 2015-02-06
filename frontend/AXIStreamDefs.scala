8e2c82d8048227c68dc466945cd4f09b2cf52aec
Yaman Umuroglu <maltanar@gmail.com>
2015-02-06 16:37:20 +0100

    derive AXIStream* from DecoupedIO instead of DeqIO and EnqIO
    it looks like there is some bug with the DeqIO and EnqIO that causes
    the signals to be not connected using the interface connection <>
    operator

--- frontend/AXIStreamDefs.scala
+++ frontend/AXIStreamDefs.scala
@@ -9,23 +9,24 @@ import Node._
 // with signal renaming to support auto inference of AXI stream interfaces in Vivado
 
 
-class AXIStreamMasterIF[T <: Data](gen: T) extends EnqIO(gen) {
+class AXIStreamMasterIF[T <: Data](gen: T) extends DecoupledIO(gen) {
   def renameSignals(ifName: String) {
     ready.setName(ifName + "_TREADY")
     valid.setName(ifName + "_TVALID")
     bits.setName(ifName + "_TDATA")
   }
-  
+
   override def clone: this.type = { new AXIStreamMasterIF(gen).asInstanceOf[this.type]; }
 }
 
-class AXIStreamSlaveIF[T <: Data](gen: T) extends DeqIO(gen) {
+class AXIStreamSlaveIF[T <: Data](gen: T) extends DecoupledIO(gen) {
+  flip()
   def renameSignals(ifName: String) {
     ready.setName(ifName + "_TREADY")
     valid.setName(ifName + "_TVALID")
     bits.setName(ifName + "_TDATA")
   }
-  
+
   override def clone: this.type = { new AXIStreamSlaveIF(gen).asInstanceOf[this.type]; }
 }
 
