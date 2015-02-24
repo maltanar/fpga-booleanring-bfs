6e14fe202cc2f422a8f995e984b24bd34bbd629c
Yaman Umuroglu <maltanar@gmail.com>
2015-02-24 15:58:26 +0100

    change frontend parameter defaults

--- frontend/Main.scala
+++ frontend/Main.scala
@@ -3,12 +3,11 @@ import Chisel._
 
 object MainObj {
   def main(args: Array[String]): Unit = {
-    val width = 1
-    val depth = 65536
+    val resultMemDepthWords = 1024 * 28
 
-    //chiselMain(args, () => Module(new FrontendController(4096)))
+    chiselMain(args, () => Module(new FrontendController(resultMemDepthWords)))
     //chiselMain(args, () => Module(new LevelGenerator(32)))
-    chiselMainTest(args, () => Module(new LevelGeneratorTestBed())) { c => new LevelGeneratorTester(c) }
+    //chiselMainTest(args, () => Module(new LevelGeneratorTestBed())) { c => new LevelGeneratorTester(c) }
     //chiselMain(args, () => Module(new StreamDeltaGen(32)))
     //chiselMainTest(args, () => Module(new StreamDeltaGenTestBed())) { c => new StreamDeltaGenTester(c) }
     //chiselMain(args, () => Module(new DualPortRAM(width, depth)))
