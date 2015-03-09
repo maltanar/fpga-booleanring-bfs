7def9546663e7c2d39a31b0025f4cc0c6288f92b
Yaman Umuroglu <maltanar@gmail.com>
2015-03-09 22:14:57 +0100

    upgrade Scala version, FrontierFilter inst example in Main

--- sparse-frontier/Main.scala
+++ sparse-frontier/Main.scala
@@ -5,7 +5,8 @@ import Chisel._
 object MainObj {
   def main(args: Array[String]): Unit = {
 
-    chiselMain(args, () => Module(new NeighborFetcher()))
+    //chiselMain(args, () => Module(new NeighborFetcher()))
+    chiselMain(args, () => Module(new FrontierFilter()))
     // chiselMainTest(args, () => Module(new NeighborFetcher())) { c => new NeighborFetcherTester(c) }
   }
 }
