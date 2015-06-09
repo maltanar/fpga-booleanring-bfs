1044d6e254429dba9fe74d081ac890308888a3e2
Yaman Umuroglu <maltanar@gmail.com>
2015-06-09 14:46:40 +0200

    add example matrix data

---
+++ example-data/rmat-16-8-csc-1/verify.tcl
@@ -0,0 +1,6 @@
+connect arm hw
+data_verify /home/maltanar/graph/rmat-16-8-csc-1/rmat-16-8-csc-0-meta.bin 0x10000400
+data_verify /home/maltanar/graph/rmat-16-8-csc-1/rmat-16-8-csc-0-indptr.bin 0x10000800
+data_verify /home/maltanar/graph/rmat-16-8-csc-1/rmat-16-8-csc-0-inds.bin 0x10041000
+data_verify /home/maltanar/graph/rmat-16-8-csc-1/config.bin 0x10000000
+disconnect 64
